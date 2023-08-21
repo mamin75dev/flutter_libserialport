5b28ff7080b6ee689aacc1075cf374ad92eacafb
mohamadamin <mamin75.dev@gmail.com>
2023-08-21 12:45:40 +0330

    delete macos and linux

--- macos/Classes/FlutterLibserialportPlugin.swift
+++
@@ -1,9 +0,0 @@
-import Cocoa
-import FlutterMacOS
-import Foundation
-
-public class FlutterLibserialportPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    setenv("LIBSERIALPORT_PATH", Bundle.main.privateFrameworksPath! + "/libserialport.framework/libserialport", 0);
-  }
-}
